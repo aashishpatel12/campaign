5b68946630fb5a0901749a0a6d15a706c526ef99
aashishpatel12 <aashishpatel86235@gmail.com>
2026-05-21 11:34:04 +0530

    second  commit

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
+// Suppress PHP 8.4+ deprecation warnings
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 // Forward Vercel requests to the normal Laravel public/index.php
 require __DIR__ . '/../public/index.php';
